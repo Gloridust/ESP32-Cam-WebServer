f1806269ecb57568a978fbab5ac5bf59a6637eae
Gloridust <Gloridust@gmail.com>
2024-06-10 23:41:00 +0800

    Update thonny2-esp32.py

--- micro-python/thonny2-esp32.py
+++ micro-python/thonny2-esp32.py
@@ -2,9 +2,7 @@ import socket
 import network
 import camera
 import time
-import os
-import sdcard
-import machine
+
 
 # 连接wifi
 wlan = network.WLAN(network.STA_IF)
@@ -12,16 +10,12 @@ wlan.active(True)
 if not wlan.isconnected():
     print('connecting to network...')
     wlan.connect('GloriStudioJD', 'qwer1234')
+    
     while not wlan.isconnected():
         pass
 print('network config:', wlan.ifconfig())
-
-# 初始化SD卡
-sd = sdcard.SDCard(machine.SPI(1), machine.Pin(15))
-vfs = os.VfsFat(sd)
-os.mount(vfs, "/sd")
-print("SD card initialized and mounted.")
-
+ 
+ 
 # 摄像头初始化
 try:
     camera.init(0, format=camera.JPEG)
@@ -29,40 +23,62 @@ except Exception as e:
     camera.deinit()
     camera.init(0, format=camera.JPEG)
 
-# 其他设置
-camera.flip(1)
-camera.mirror(1)
-camera.framesize(camera.FRAME_FHD)  # 设置为1080p
-camera.speffect(camera.EFFECT_NONE)
-camera.saturation(0)
-camera.brightness(0)
-camera.contrast(0)
-camera.quality(10)
 
-# 初始化计时器和文件计数器
-start_time = time.time()
-file_count = 1
-file_path = "/sd/"
+# 其他设置：
+# 上翻下翻
+camera.flip(1)
+#左/右
+camera.mirror(1)
+
+# 分辨率
+camera.framesize(camera.FRAME_HVGA)
+# 选项如下：
+# FRAME_96X96 FRAME_QQVGA FRAME_QCIF FRAME_HQVGA FRAME_240X240
+# FRAME_QVGA FRAME_CIF FRAME_HVGA FRAME_VGA FRAME_SVGA
+# FRAME_XGA FRAME_HD FRAME_SXGA FRAME_UXGA FRAME_FHD
+# FRAME_P_HD FRAME_P_3MP FRAME_QXGA FRAME_QHD FRAME_WQXGA
+# FRAME_P_FHD FRAME_QSXGA
+
+# 特效
+camera.speffect(camera.EFFECT_NONE)
+#选项如下：
+# 效果\无（默认）效果\负效果\ BW效果\红色效果\绿色效果\蓝色效果\复古效果
+# EFFECT_NONE (default) EFFECT_NEG \EFFECT_BW\ EFFECT_RED\ EFFECT_GREEN\ EFFECT_BLUE\ EFFECT_RETRO
+
+# 白平衡
+# camera.whitebalance(camera.WB_HOME)
+#选项如下：
+# WB_NONE (default) WB_SUNNY WB_CLOUDY WB_OFFICE WB_HOME
+
+# 饱和
+camera.saturation(0)
+#-2,2（默认为0）. -2灰度
+# -2,2 (default 0). -2 grayscale 
+
+# 亮度
+camera.brightness(0)
+#-2,2（默认为0）. 2亮度
+# -2,2 (default 0). 2 brightness
+
+# 对比度
+camera.contrast(0)
+#-2,2（默认为0）.2高对比度
+#-2,2 (default 0). 2 highcontrast
+
+# 质量
+camera.quality(10)
+#10-63数字越小质量越高
+
+# socket UDP 的创建
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,0)
 
 try:
     while True:
-        current_time = time.time()
-        elapsed_time = current_time - start_time
-        
-        # 每分钟保存一个新的视频文件
-        if elapsed_time >= 60:
-            file_count += 1
-            start_time = current_time
-        
         buf = camera.capture()  # 获取图像数据
-        file_name = "{:05d}.jpg".format(file_count)
-        with open(file_path + file_name, "wb") as f:
-            f.write(buf)
-        
+        s.sendto(buf, ("192.168.68.175", 9090))  # 向服务器发送图像数据
         time.sleep(0.1)
-except Exception as e:
-    print("An error occurred:", e)
+except:
+    pass
 finally:
     camera.deinit()
-    os.umount("/sd")
-    print("Camera deinitialized and SD card unmounted.")
+
